e2a42a76bf1c2d39fbeab5f19c5d7378db29e33d
Unknown <brendan.furneaux@gmail.com>
2020-10-14 15:37:54 +0200

    avoid errors and warnings from magrittry pipe and dot

---
+++ R/phylotax-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+# commented out because of roxygen bug when there is a package and function of
+# same name
+#"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom magrittr %>%
+## usethis namespace: end
+#' @importFrom rlang .data
+NULL
+
+# get rid of CRAN notes
+utils::globalVariables(c("."))
